1ffb3ee1c058626c7b36e55d9f7ae3b5ed8a65d2
Maxim Sorokin <maxim.sorokin@gmail.com>
2015-10-20 15:47:56 +0300

    I'm greate!

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! I'm great!");
     }
     
 }
